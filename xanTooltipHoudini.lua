d033fb4ca5f95a6486d8ad8fef5fdb672feb5297
Xruptor <xruptor@manaflux.com>
2018-09-26 15:44:16 -0400

    A lot of fixes and updates to the code to make things a bit more smoother.

--- xanTooltipHoudini.lua
+++ xanTooltipHoudini.lua
@@ -40,34 +40,25 @@ end
 ----------------------
 
 local function processAuraTooltip(self, unitid, index, filter)
-	auraSwitch = true
+	if unitid == "player" then
+		auraSwitch = true
+		return
+	end
+	auraSwitch = false
 end
-
---this is to prevent spamming check of the tooltip.
-local lastTooltipTarget = ""
 
 local function checkPlayerQuest()
 	for i=1,GameTooltip:NumLines() do
 		local ttText = getglobal("GameTooltipTextLeft" .. i)
-		if ttText and ttText:GetText() and lastTooltipTarget == ttText:GetText() then 
-			return true
-		else
-			lastTooltipTarget = ""
-		end
-		if ttText and ttText:GetText() and playerQuests[ttText:GetText()] ~= nil then
-			local ttTextCache = getglobal("GameTooltipTextLeft1")
-			if ttTextCache and ttTextCache:GetText() then
-				lastTooltipTarget = ttTextCache:GetText()
-			end
+		if ttText and ttText:GetText() and playerQuests[ttText:GetText()] then
 			return true
 		end
 	end
-	lastTooltipTarget = ""
 	return false
 end
 
 function f:doQuestTitleGrab()
-
+	playerQuests = {}
 	for i=1,GetNumQuestLogEntries() do
 		local questTitle, _, _, _, isHeader = GetQuestLogTitle(i)
 		
@@ -75,9 +66,41 @@ function f:doQuestTitleGrab()
 			playerQuests[questTitle] = questTitle
 		end
 	end
+end
 
-	--reset
-	lastTooltipTarget = ""
+function f:InCombatLockdown()
+	return InCombatLockdown() or UnitAffectingCombat("player")
+end
+
+function f:CheckTooltipStatus(tooltip, unit)
+	if not f:InCombatLockdown() then return end
+	if not XTH_DB then return end
+	
+	local name = tooltip:GetName() and string.lower(tooltip:GetName()) or ""
+
+	--this is for the special buffs/debuffs icons above the nameplates, units are nameplate1, nameplate2, etc...
+	if unit and string.find(unit, "nameplate") then
+		tooltip:Hide()
+		return
+	elseif tooltip == NamePlateTooltip or string.find(name, "nameplate") then
+		--we really don't want to do anything else with nameplate
+		return
+	end
+	
+	local owner = tooltip:GetOwner()
+	local ownerName = owner and owner:GetParent() and owner:GetParent():GetName()
+
+	if XTH_DB.showAuras then
+		if auraSwitch then return end
+		if ownerName and ownerName == "BuffFrame" then return end
+	end
+	
+	if XTH_DB.showQuestObj and checkPlayerQuest() then return end
+	if ownerName and ignoreFrames[ownerName] then return end
+	
+	if not IsShiftKeyDown() then
+		tooltip:Hide()
+	end
 end
 
 function f:PLAYER_LOGIN()
@@ -119,95 +142,49 @@ function f:PLAYER_LOGIN()
 		DEFAULT_CHAT_FRAME:AddMessage("/xth quest - toggles Quest objective tooltips (ON/OFF)")
 	end
 
-	GameTooltip:HookScript("OnShow", function(self)
-		local canPass = false
-		if XTH_DB and not XTH_DB.showAuras then canPass = true end
-		if InCombatLockdown() and XTH_DB and XTH_DB.showQuestObj and checkPlayerQuest() then canPass = true end --we don't want to constantly scan tooltip, only in combat
-		
-		if XTH_DB and canPass then
-			if InCombatLockdown() then
-				local owner = self:GetOwner()
-				if not XTH_DB.showAuras and owner and owner:GetParent() and owner:GetParent():GetName() and ignoreFrames[owner:GetParent():GetName()] then
-					--do nothing
-					return
-				end
-				if XTH_DB.showQuestObj and checkPlayerQuest() then
-					--do nothing
-					return
-				end
-				if not XTH_DB.showAuras or not XTH_DB.showQuestObj then
-					if not IsShiftKeyDown() then
-						self:Hide()
-					end
-				end
-				return
-			end
-		end
+	-------
+	-------GameTooltip
+	-------
+	
+	GameTooltip:HookScript("OnShow", function(objTooltip)
+		f:CheckTooltipStatus(objTooltip)
+	end)
+	
+	GameTooltip:HookScript("OnUpdate", function(objTooltip, elapsed)
+		f:CheckTooltipStatus(objTooltip)
 	end)
 	
 	--check if it's one of those new small buff icons that show ontop of the target mob nameplate
-	hooksecurefunc(GameTooltip,"SetUnitAura",function(self,unit,index,filter)
-		--local caster = select(8,UnitAura(unit,index,filter))
-		if InCombatLockdown() and string.find(unit, "nameplate") then
-			self:Hide()
-		end
+	hooksecurefunc(GameTooltip,"SetUnitAura",function(objTooltip, unit, index, filter)
+		processAuraTooltip(objTooltip, unit, index, filter)
+		f:CheckTooltipStatus(objTooltip, unit)
 	end)
 
-	GameTooltip:HookScript("OnUpdate", function(self, elapsed)
-		local canPass = false
-		if XTH_DB and XTH_DB.showAuras then canPass = true end
-		if InCombatLockdown() and XTH_DB and XTH_DB.showQuestObj and checkPlayerQuest() then canPass = true end --we don't want to constantly scan tooltip, only in combat
-		
-		if XTH_DB and canPass and self:IsShown() then
-			local owner = self:GetOwner()
-			if InCombatLockdown() then
-				--check for temporary enchant frame
-				if XTH_DB.showAuras and not auraSwitch and owner and owner:GetParent() and owner:GetParent():GetName() and ignoreFrames[owner:GetParent():GetName()] then
-					--do nothing
-					return
-				end
-				if XTH_DB.showQuestObj and checkPlayerQuest() then
-					--do nothing
-					return
-				end
-				--otherwise hide it
-				if not auraSwitch or not XTH_DB.showQuestObj then
-					if not IsShiftKeyDown() then
-						self:Hide()
-					end
-				end
-			end
-		end
-	end)
-	
-	-- GameTooltip:SetScript("OnTooltipSetQuest", function(self, ...)
-		-- if XTH_DB.showQuestObj then
-			-- checkPlayerQuest()
-		-- end
-    -- end)
-	
-	-- GameTooltip:SetScript("OnTooltipSetUnit", function(self, ...)
-		-- if XTH_DB.showQuestObj then
-			-- checkPlayerQuest()
-		-- end
-    -- end)
-	
-	-- GameTooltip:SetScript("OnTooltipSetItem", function(self, ...)
-		-- if XTH_DB.showQuestObj then
-			-- checkPlayerQuest()
-		-- end
-    -- end)
-	
 	GameTooltip:HookScript("OnHide", function(self)
 		auraSwitch = false
 	end)
-
 	
-	--decide if we want to show aura tooltips while in battle
-	hooksecurefunc(GameTooltip, "SetUnitAura", processAuraTooltip)
 	hooksecurefunc(GameTooltip, "SetUnitBuff", processAuraTooltip)
 	hooksecurefunc(GameTooltip, "SetUnitDebuff", processAuraTooltip)
-
+	
+	-------
+	-------NamePlateTooltip
+	-------
+	
+	--NamePlateTooltip that shows above nameplate with the buff/debuffs
+	NamePlateTooltip:HookScript("OnShow", function(objTooltip)
+		f:CheckTooltipStatus(objTooltip)
+	end)
+	
+	NamePlateTooltip:HookScript("OnUpdate", function(objTooltip, elapsed)
+		f:CheckTooltipStatus(objTooltip)
+	end)
+	
+	--check if it's one of those new small buff icons that show ontop of the target mob nameplate
+	hooksecurefunc(NamePlateTooltip,"SetUnitAura",function(objTooltip, unit, index, filter)
+		f:CheckTooltipStatus(objTooltip, unit)
+	end)
+	
 	--activate triggers
 	self:RegisterEvent("QUEST_COMPLETE")
 	self:RegisterEvent("UNIT_QUEST_LOG_CHANGED")
